d2cacfcd7c522d65d963651f44a4f23986bb3edc
BatataRaiz <joaodeamo3@gmail.com>
2023-09-19 20:51:16 -0300

    Flask

--- src/jvdeamo/test.py
+++ src/jvdeamo/test.py
@@ -7,7 +7,13 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def welcome_message():
-    return "Seja bem-vindo(a) ao App Senai!"
+    return "Bem-vindo(a) ao App Senai!"
+
+def redirect_to_menu():
+    return redirect(url_for('menu'))
+
+def redirect_to_exit():
+    return redirect(url_for('exit'))
 
 @app.route('/')
 def index():
@@ -16,13 +22,19 @@ def index():
 @app.route('/iniciar', methods=['POST'])
 def iniciar():
     clear_screen()
-    welcome_msg = welcome_message()
-    entrar = request.form.get('entrada')
     
-    if entrar == '1':
-        return render_template('menu.html', message=welcome_msg)
-    else:
-        return "Software encerrado!"
+    if 'nome' in request.form:
+        return redirect_to_menu()
+    elif 'sair' in request.form:
+        return redirect_to_exit()
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html', message=welcome_message())
+
+@app.route('/exit', methods=['GET', 'POST'])
+def exit():
+    return render_template('exit.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
